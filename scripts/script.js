6ba36e8ad1f98a01ee1062625c8b4785568bdef2
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-05-14 00:19:13 +0200

    with dots

--- scripts/script.js
+++ scripts/script.js
@@ -13,8 +13,33 @@ tabsArray.forEach(tab => {
             pageDots: false,
             prevNextButtons: false
         });
+        registerDotListeners();
     });
 });
+
+function registerDotListeners() {
+    const firstDot = document.querySelector('.dot-F');
+    const secondDot = document.querySelector('.dot-S');
+    const thirdDot = document.querySelector('.dot-T');
+    firstDot.addEventListener('click', e => {
+        initialized.select(0);
+        firstDot.classList.add('active');
+        secondDot.classList.remove('active');
+        thirdDot.classList.remove('active');
+    });
+    secondDot.addEventListener('click', () => {
+        initialized.select(1);
+        secondDot.classList.add('active');
+        firstDot.classList.remove('active');
+        thirdDot.classList.remove('active');
+    });
+    thirdDot.addEventListener('click', () => {
+        initialized.select(2);
+        thirdDot.classList.add('active');
+        firstDot.classList.remove('active');
+        secondDot.classList.remove('active');
+    });
+}
 
 // 'use strict';
 // const tabs = document.querySelectorAll('.nav-item');
