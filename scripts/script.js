ddc0c655c61d2798a7037695de0a918c89c93ba9
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-05-14 00:14:32 +0200

    newone

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-'use strict';
 const tabs = document.querySelectorAll('.nav-item');
 const tabsArray = [...tabs];
 
@@ -12,33 +11,52 @@ tabsArray.forEach(tab => {
         }
         initialized = new Flickity(`#${id}`, {
             pageDots: false,
-            prevNextButtons: false,
-            setGallerySize: false
+            prevNextButtons: false
         });
-        registerDotListeners()
     });
 });
 
-function registerDotListeners() {
-    const firstDot = document.querySelector('.dot-F');
-    const secondDot = document.querySelector('.dot-S');
-    const thirdDot = document.querySelector('.dot-T');
-    firstDot.addEventListener('click', e => {
-        initialized.select(0);
-        firstDot.classList.add('active');
-        secondDot.classList.remove('active');
-        thirdDot.classList.remove('active');
-    });
-    secondDot.addEventListener('click', () => {
-        initialized.select(1);
-        secondDot.classList.add('active');
-        firstDot.classList.remove('active');
-        thirdDot.classList.remove('active');
-    });
-    thirdDot.addEventListener('click', () => {
-        initialized.select(2);
-        thirdDot.classList.add('active');
-        firstDot.classList.remove('active');
-        secondDot.classList.remove('active');
-    });
-}
+// 'use strict';
+// const tabs = document.querySelectorAll('.nav-item');
+// const tabsArray = [...tabs];
+
+// var initialized = null;
+
+// tabsArray.forEach(tab => {
+//     tab.addEventListener('click', (e) => {
+//         const id = `carousel-${e.target.id}`;
+//         if (initialized) {
+//             initialized.destroy();
+//         }
+//         initialized = new Flickity(`#${id}`, {
+//             pageDots: false,
+//             prevNextButtons: false,
+//             setGallerySize: false
+//         });
+//         registerDotListeners()
+//     });
+// });
+
+// function registerDotListeners() {
+//     const firstDot = document.querySelector('.dot-F');
+//     const secondDot = document.querySelector('.dot-S');
+//     const thirdDot = document.querySelector('.dot-T');
+//     firstDot.addEventListener('click', e => {
+//         initialized.select(0);
+//         firstDot.classList.add('active');
+//         secondDot.classList.remove('active');
+//         thirdDot.classList.remove('active');
+//     });
+//     secondDot.addEventListener('click', () => {
+//         initialized.select(1);
+//         secondDot.classList.add('active');
+//         firstDot.classList.remove('active');
+//         thirdDot.classList.remove('active');
+//     });
+//     thirdDot.addEventListener('click', () => {
+//         initialized.select(2);
+//         thirdDot.classList.add('active');
+//         firstDot.classList.remove('active');
+//         secondDot.classList.remove('active');
+//     });
+// }
