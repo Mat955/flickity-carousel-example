1b263e09e4679daa2a5388e26f65e4753636e2a0
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-05-13 21:37:56 +0200

    #2version

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,42 @@
 'use strict';
-// const tabs = document.querySelectorAll('.nav-item');
-// const tabsArray = [...tabs];
-// tabsArray.forEach(tab => {
-//     tab.addEventListener('click', (e) => {
-//         const id = `carousel-${e.target.id}`;
-//         var productCarousel = new Flickity(`#${id}`, {
-//             pageDots: false,
-//             prevNextButtons: false,
-//             setGallerySize: false
-//         });
-//         registerDotListener(productCarousel);
-//     });
-// });
+const tabs = document.querySelectorAll('.nav-item');
+const tabsArray = [...tabs];
 
-var productCarousel = new Flickity('.carousel-1', {
-    pageDots: false,
-    prevNextButtons: false,
-    setGallerySize: false
+var initialized = null;
+
+tabsArray.forEach(tab => {
+    tab.addEventListener('click', (e) => {
+        const id = `carousel-${e.target.id}`;
+        if (initialized) {
+            initialized.destroy();
+        }
+        initialized = new Flickity(`#${id}`, {
+            pageDots: false,
+            prevNextButtons: false,
+            setGallerySize: false
+        });
+        registerDotListeners()
+    });
 });
-registerDotListener(productCarousel);
 
-function registerDotListener(productCarousel) {
+function registerDotListeners() {
     const firstDot = document.querySelector('.dot-F');
     const secondDot = document.querySelector('.dot-S');
     const thirdDot = document.querySelector('.dot-T');
     firstDot.addEventListener('click', e => {
-        productCarousel.select(0);
+        initialized.select(0);
         firstDot.classList.add('active');
         secondDot.classList.remove('active');
         thirdDot.classList.remove('active');
     });
     secondDot.addEventListener('click', () => {
-        productCarousel.select(1);
+        initialized.select(1);
         secondDot.classList.add('active');
         firstDot.classList.remove('active');
         thirdDot.classList.remove('active');
     });
     thirdDot.addEventListener('click', () => {
-        productCarousel.select(2);
+        initialized.select(2);
         thirdDot.classList.add('active');
         firstDot.classList.remove('active');
         secondDot.classList.remove('active');
